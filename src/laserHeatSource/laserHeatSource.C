ec61b0df2d60148f5772565f863ca4a25638f20f
Petar Cosic <cosicar@dhcp-892b9485.ucd.ie>
2025-11-10 14:50:54 +0000

    testing

--- src/laserHeatSource/laserHeatSource.C
+++ src/laserHeatSource/laserHeatSource.C
@@ -151,6 +151,9 @@ void laserHeatSource::createInitialRays
     }
     else // One ray for each boundary patch face within the laser radius
     {
+
+
+        
         const vectorField& CI = mesh.C();
 
         forAll(CI, celli)
