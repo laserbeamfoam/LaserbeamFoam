d4134e125a79c8cf4701f4ca3cb723166a3a8820
cpetar112@gmail.com <cpetar112@gmail.com>
2025-11-13 17:02:14 +0000

    reverting

--- src/laserHeatSource/laserHeatSource.C
+++ src/laserHeatSource/laserHeatSource.C
@@ -425,9 +425,9 @@ laserHeatSource::laserHeatSource
             continue;
         }
 
-        const label localMax = patchField.size() ? max(patchField) : -1e+300;
+        const scalar localMax = patchField.size() ? max(patchField) : -1e+300;
 
-        if (localMax > 0)
+        if (localMax > 0.9)
         {
             localLaserPatchName = patchName;
         }
